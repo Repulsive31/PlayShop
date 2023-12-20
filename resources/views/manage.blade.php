9107c01b4eab48828240650eae18981a7f324492
Repulsive31 <91827275+Repulsive31@users.noreply.github.com>
2023-12-21 01:47:43 +0700

    v 1.4

---
+++ resources/views/manage.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Item Management</title>
+    
+    <link href="/css/index.css" rel="stylesheet">
+</head>
+<body>
+    <div class="manageTitle">
+        <h1>Item Management</h1>
+        <hr>
+    </div>
+    <div class="manageContainer">
+        <div class="manageFirst">
+            <div class="manageImg">
+                <a href="#">
+                    <p>Add Images</p>
+                </a>
+            </div>
+            <div class="manageItemId">
+                <p>Item ID: PRDCT0001</p>
+            </div>
+        </div>
+        <div class="manageSecond">
+            <div class="manageFirstInfo">
+                <p>Item Name:</p>
+                <p>Item Category:</p>
+                <p>Item Information:</p>
+                <div class="manageSecondInfo">
+                    <p>Release Date:</p>
+                    <p>Sumber Pengiriman:</p>
+                    <p>Stock tersisa:</p>
+                    <p>Harga barang:</p>
+                </div>
+                <div class="manageItemDesc">
+                    <h3>Item Description</h3>
+                    <p>Lorem Ipsum bla bla bla
+                        Lorem Ipsum bla bla bla
+                        Lorem Ipsum bla bla bla
+                        Lorem Ipsum bla bla bla
+                        Lorem Ipsum bla bla bla
+                        Lorem Ipsum bla bla bla
+                        Lorem Ipsum bla bla bla</p>
+                </div>
+                <div class="manageButtons">
+                    <button>Add Item</button>
+                    <button>Edit Item</button>
+                    <button>Remove Item</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('list');
+    return view('manage');
 });
 
 Auth::routes();
