39d81eaf9ecf2a0a54bc3bf14ebff83996ae5848
Repulsive31 <91827275+Repulsive31@users.noreply.github.com>
2023-12-20 20:49:19 +0700

    v 1.2

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -19,26 +19,70 @@
             </div>
             <hr>
         </div>
-        <div class="cartCard">
+    </div>
+    <div class="cartContent">
+        <div class="cartContentLabel">
             <div class="checkBtn">
-                <img src="/imgs/uncheckedBox.svg" alt="checked">
+                <button>
+                    <img src="/imgs/uncheckedBox.svg" alt="checked">
+                </button>
             </div>
-            <div class="cartProduct">
-                <div class="cartImg">
-                    <img src="/imgs/mouse-2-100.svg" alt="">
-                </div>
-                <div class="cartProductInfo">
-                    <p>> Mouse Name</p>
-                    <p>Jakarta</p>
-                    <h3>Rp. 100.000</h3>
-                </div>
-                <div class="cartExtra">
-                    + -
+            <div class="cartCard">
+                <div class="cartProduct">
+                    <div class="cartImg">
+                        <img src="/imgs/mouse-2-100.svg" alt="">
+                    </div>
+                    <div class="cartProductInfo">
+                        <p>> Mouse Name</p>
+                        <p>Jakarta</p>
+                        <h3>Rp. 100.000</h3>
+                    </div>
+                    <div class="cartExtra">
+                        <div>
+                            <form action="/increment" method="post">
+                                @csrf
+                                <button type="submit">+</button>
+                            </form>
+                            <form action="/decrement" method="post">
+                                @csrf
+                                <button type="submit">-</button>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="cardCheckOut">
-            
+        <div class="cardCheckout">
+            <h1>Checkout</h1>
+            <div class="checkoutUser">
+                <p>Pelanggan: </p>
+                <p>Tanggal: </p>
+            </div>
+            <div class="checkoutDesc">
+                <div class="checkoutDescTop">
+                    <p>No.</p>
+                    <p>Kode Barang</p>
+                    <p>Qty</p>
+                    <p>Harga</p>
+                    <p>Total</p>
+                </div>
+                <div class="checkoutLine">
+                    <hr>
+                </div>
+                <div class="checkoutProduct">
+                        <p>asdasdasdas</p>
+                </div>
+                <div class="checkoutLine">
+                    <hr>
+                </div>
+                <div class="checkoutDescBot">
+                    <p>Total:</p>
+                    <p>{price}</p>
+                </div>
+                <div class="checkoutBtn">
+                    <button>Lanjut</button>
+                </div>
+            </div>
         </div>
     </div>
 </body>
